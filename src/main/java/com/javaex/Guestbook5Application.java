e10f0d9359f1874af0e1ecfbacf0ef9f5a2cefca
pjh990909 <pjh990909@naver.com>
2024-03-06 00:30:13 +0900

    과제끝

---
+++ src/main/java/com/javaex/Guestbook5Application.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guestbook5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guestbook5Application.class, args);
+	}
+
+}
